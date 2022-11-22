bda94db19bcb452c32f4c77362911dac18ce59ba
Bolych <115889159+Bolych@users.noreply.github.com>
2022-11-22 15:56:38 +0400

    Update app.js

--- app.js
+++ app.js
@@ -122,12 +122,38 @@ function numerologyAppear() {
   }, 700);
   setTimeout(function () {
     textAfterQuestion.classList.toggle("white");
-  }, 2000);
+  }, 1500);
 }
 
 btnYes.addEventListener("click", numerologyAppear);
 btnNo.addEventListener("click", numerologyAppear);
 
-// console.log(btnNo);
-// console.log(btnYes);
-console.log(pushMessage);
+// carousel
+
+const slider = document.querySelector(".carousel-slide");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
